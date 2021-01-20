22fe76c9080789f59c316d8f3b4942a56e750558
Jake Walsh <37653943+jakewalsh90@users.noreply.github.com>
2021-01-20 20:23:26 +0000

    Update provider.tf

--- Automatic-ClientIP-NSG/provider.tf
+++ Automatic-ClientIP-NSG/provider.tf
@@ -2,7 +2,6 @@
 terraform {
   required_providers {
     azurerm = {
-      # Specify what version of the provider we are going to utilise
       source = "hashicorp/azurerm"
       version = ">= 2.43.0"
     }

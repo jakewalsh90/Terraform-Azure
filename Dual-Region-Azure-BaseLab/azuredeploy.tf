fafda39380a2442de56d633d1b075441002c8cbb
Jake Walsh <37653943+jakewalsh90@users.noreply.github.com>
2021-01-22 17:03:50 +0000

    added data item for client IP check and NSG rule for RDP in for both regions

--- Dual-Region-Azure-BaseLab/azuredeploy.tf
+++ Dual-Region-Azure-BaseLab/azuredeploy.tf
@@ -211,3 +211,53 @@ resource "azurerm_subnet" "region2-vnet2-snet3" {
         }    
     }
 }
+#RDP Access Rules for Lab
+#Get Client IP Address for NSG
+data "http" "clientip" {
+  url = "https://ipv4.icanhazip.com/"
+}
+#Lab NSGs
+#Region 1
+resource "azurerm_network_security_group" "region1-nsg" {
+  name                = "region1-nsg"
+  location            = var.loc1
+  resource_group_name = azurerm_resource_group.rg2.name
+
+  security_rule {
+    name                       = "RDP-In"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "3389"
+    source_address_prefix      = "${chomp(data.http.clientip.body)}/32"
+    destination_address_prefix = "*"
+  }
+   tags     = {
+       Environment  = var.environment_tag
+       Function = "baselabv2-security"
+   }
+}
+#Region 2
+resource "azurerm_network_security_group" "region2-nsg" {
+  name                = "region2-nsg"
+  location            = var.loc2
+  resource_group_name = azurerm_resource_group.rg3.name
+
+  security_rule {
+    name                       = "RDP-In"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "3389"
+    source_address_prefix      = "${chomp(data.http.clientip.body)}/32"
+    destination_address_prefix = "*"
+  }
+   tags     = {
+       Environment  = var.environment_tag
+       Function = "baselabv2-security"
+   }
+}

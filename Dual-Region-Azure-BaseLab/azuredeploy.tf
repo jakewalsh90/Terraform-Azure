e847460e6e821205f4782e60c016d56004a442e4
Jake Walsh <37653943+jakewalsh90@users.noreply.github.com>
2021-01-23 20:04:25 +0000

    added keyvault

--- Dual-Region-Azure-BaseLab/azuredeploy.tf
+++ Dual-Region-Azure-BaseLab/azuredeploy.tf
@@ -304,3 +304,55 @@ resource "azurerm_subnet_network_security_group_association" "region2-vnet2-snet
   subnet_id                 = azurerm_subnet.region2-vnet2-snet2.id
   network_security_group_id = azurerm_network_security_group.region2-nsg.id
 }
+#Create KeyVault ID
+resource "random_id" "kvname" {
+  byte_length = 5
+  prefix = "keyvault"
+}
+#Keyvault Creation
+data "azurerm_client_config" "current" {}
+resource "azurerm_key_vault" "kv1" {
+  depends_on = [ azurerm_resource_group.rg2 ]
+  name                        = random_id.kvname.hex
+  location                    = var.loc1
+  resource_group_name         = var.azure-rg-2
+  enabled_for_disk_encryption = true
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
+
+  sku_name = "standard"
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+    key_permissions = [
+      "get",
+    ]
+
+    secret_permissions = [
+      "get", "backup", "delete", "list", "purge", "recover", "restore", "set",
+    ]
+
+    storage_permissions = [
+      "get",
+    ]
+  }
+   tags     = {
+       Environment  = var.environment_tag
+       Function = "baselabv2-security"
+   }
+}
+#Create KeyVault VM password
+resource "random_password" "vmpassword" {
+  length = 20
+  special = true
+}
+#Create Key Vault Secret
+resource "azurerm_key_vault_secret" "vmpassword" {
+  name         = "vmpassword"
+  value        = random_password.vmpassword.result
+  key_vault_id = azurerm_key_vault.kv1.id
+  depends_on = [ azurerm_key_vault.kv1 ]
+}

33c8d9ad2ccc0d19cc11a804b54b452d0bac803e
Jake Walsh <37653943+jakewalsh90@users.noreply.github.com>
2021-02-15 20:36:36 +0000

    updated scripts for repo URL

--- Single-Region-Azure-BaseLab-with-Ansible/azuredeploy.tf
+++ Single-Region-Azure-BaseLab-with-Ansible/azuredeploy.tf
@@ -303,7 +303,7 @@ resource "azurerm_virtual_machine_extension" "region1-dc01-basesetup" {
   settings = <<SETTINGS
     {
         "fileUris": [
-          "https://raw.githubusercontent.com/jakewalsh90/Terraform-Azure/main/Single-Region-Azure-BaseLab/PowerShell/baselab_DCSetup.ps1"
+          "https://raw.githubusercontent.com/jakewalsh90/Terraform-Azure/main/Single-Region-Azure-BaseLab-with-Ansible/PowerShell/baselab_DCSetup.ps1"
         ]
     }
   SETTINGS

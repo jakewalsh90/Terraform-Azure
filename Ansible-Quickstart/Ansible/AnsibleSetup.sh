db1d4a133d86a27895a7475bc9b66c152561d2fc
Jake Walsh <37653943+jakewalsh90@users.noreply.github.com>
2021-02-19 14:57:29 +0000

    tweaks to ansible setup

--- Ansible-Quickstart/Ansible/AnsibleSetup.sh
+++ Ansible-Quickstart/Ansible/AnsibleSetup.sh
@@ -4,3 +4,5 @@ sudo apt-get upgrade -y
 sudo apt-get install python3-pip -y
 sudo pip3 install 'ansible[azure]'
 ansible-galaxy collection install azure.azcollection
+wget https://raw.githubusercontent.com/ansible-collections/azure/dev/requirements-azure.txt
+sudo pip3 install -r requirements-azure.txt

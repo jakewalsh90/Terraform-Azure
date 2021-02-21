5835de6be7411d3b14465e4430a1624c2f69c18c
Jake Walsh <37653943+jakewalsh90@users.noreply.github.com>
2021-02-21 09:52:03 +0000

    updated setup script

--- Ansible-Quickstart/Ansible/AnsibleSetup.sh
+++ Ansible-Quickstart/Ansible/AnsibleSetup.sh
@@ -10,9 +10,3 @@ sudo pip3 install 'ansible[azure]'
 ansible-galaxy collection install azure.azcollection
 wget https://raw.githubusercontent.com/ansible-collections/azure/dev/requirements-azure.txt
 sudo pip3 install -r requirements-azure.txt
-#install python-pip
-sudo apt-get install python-pip -y
-#install ansible azure preview modules
-ansible-galaxy install azure.azure_preview_modules
-#install preview modules
-sudo pip install -r ~/.ansible/roles/azure.azure_preview_modules/files/requirements-azure.txt

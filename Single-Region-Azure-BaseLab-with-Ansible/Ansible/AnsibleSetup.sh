be85ab9fddcd14031f10a51612a3fc27d511b45a
Jake Walsh <37653943+jakewalsh90@users.noreply.github.com>
2021-02-22 08:17:35 +0000

    update ansible setup script

--- Single-Region-Azure-BaseLab-with-Ansible/Ansible/AnsibleSetup.sh
+++ Single-Region-Azure-BaseLab-with-Ansible/Ansible/AnsibleSetup.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+#update VM
 sudo apt-get update
 sudo apt-get upgrade -y
+#install python3-pip
 sudo apt-get install python3-pip -y
+#install ansible
 sudo pip3 install 'ansible[azure]'
+#install ansible azure modules and plugins
 ansible-galaxy collection install azure.azcollection
-wget https://raw.githubusercontent.com/jakewalsh90/Terraform-Azure/main/Single-Region-Azure-BaseLab-with-Ansible/Ansible/requirements-azure.txt
+wget https://raw.githubusercontent.com/ansible-collections/azure/dev/requirements-azure.txt
 sudo pip3 install -r requirements-azure.txt

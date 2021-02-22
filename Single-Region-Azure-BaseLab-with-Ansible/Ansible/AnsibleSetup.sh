626949533fbae783762ea85b2b33b69b1bc77f50
Jake Walsh <37653943+jakewalsh90@users.noreply.github.com>
2021-02-22 10:01:07 +0000

    setup script tweaks

--- Single-Region-Azure-BaseLab-with-Ansible/Ansible/AnsibleSetup.sh
+++ Single-Region-Azure-BaseLab-with-Ansible/Ansible/AnsibleSetup.sh
@@ -1,12 +1,7 @@
-#!/bin/bash
-#update VM
 sudo apt-get update
 sudo apt-get upgrade -y
-#install python3-pip
 sudo apt-get install python3-pip -y
-#install ansible
 sudo pip3 install 'ansible[azure]'
-#install ansible azure modules and plugins
 ansible-galaxy collection install azure.azcollection
 wget https://raw.githubusercontent.com/ansible-collections/azure/dev/requirements-azure.txt
 sudo pip3 install -r requirements-azure.txt

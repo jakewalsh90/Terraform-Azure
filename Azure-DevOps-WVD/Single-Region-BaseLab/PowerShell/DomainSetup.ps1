e14c9534574e876e221431e4566578349c48bbf4
Jake Walsh <37653943+jakewalsh90@users.noreply.github.com>
2021-03-22 17:01:59 +0000

    file changes

--- Azure-DevOps-WVD/Single-Region-BaseLab/PowerShell/DomainSetup.ps1
+++ Azure-DevOps-WVD/Single-Region-BaseLab/PowerShell/DomainSetup.ps1
@@ -1,3 +1,4 @@
+$password = ConvertTo-SecureString '__vmpassword__' -AsPlainText -Force
 Import-Module ADDSDeployment
 Install-ADDSForest `
 -CreateDnsDelegation:$false `
@@ -10,4 +11,5 @@ Install-ADDSForest `
 -LogPath "E:\windows\NTDS" `
 -NoRebootOnCompletion:$false `
 -SysvolPath "E:\windows\SYSVOL" `
--Force:$true
+-Force:$true `
+-SafeModeAdministratorPassword: $password

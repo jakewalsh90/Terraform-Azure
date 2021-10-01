becab324d8c6737886641626b3a2f0bd4d15f57e
Jake Walsh <37653943+jakewalsh90@users.noreply.github.com>
2021-10-01 12:37:13 +0100

    provider updates Oct 2021

--- Single-Region-Azure-BaseLab/provider.tf
+++ Single-Region-Azure-BaseLab/provider.tf
@@ -4,11 +4,11 @@ terraform {
     azurerm = {
       # Specify what version of the provider we are going to utilise
       source  = "hashicorp/azurerm"
-      version = ">= 2.43.0"
+      version = ">= 2.79.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = "3.0.1"
+      version = "3.1.0"
     }
   }
 }

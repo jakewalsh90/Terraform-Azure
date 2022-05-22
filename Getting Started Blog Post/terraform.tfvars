6a01c1ad21c4c5ee018034fd1b3ccd85dab7a481
Jake Walsh <37653943+jakewalsh90@users.noreply.github.com>
2022-05-22 12:45:16 +0100

    Getting Started Blog Files

---
+++ Getting Started Blog Post/terraform.tfvars
@@ -0,0 +1,4 @@
+azure-rg-1                  = "rg-demo-lab"
+loc1                        = "uk south"
+region1-vnet1-name          = "region1-vnet1-hub1"
+region1-vnet1-address-space = "10.10.0.0/16"
---
+++ Getting Started Blog Post/variables.tf
@@ -0,0 +1,16 @@
+variable "azure-rg-1" {
+  type        = string
+  description = "resource group 1"
+}
+variable "loc1" {
+  description = "The location for this Lab environment"
+  type        = string
+}
+variable "region1-vnet1-name" {
+  description = "VNET1 Name"
+  type        = string
+}
+variable "region1-vnet1-address-space" {
+  description = "VNET address space"
+  type        = string
+}

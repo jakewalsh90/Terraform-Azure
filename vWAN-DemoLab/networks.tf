ffcffc0ee658c863042bd83c406003b51e510fa2
Jake Walsh <37653943+jakewalsh90@users.noreply.github.com>
2022-06-10 16:24:09 +0100

    terraform fmt

--- vWAN-DemoLab/networks.tf
+++ vWAN-DemoLab/networks.tf
@@ -9,7 +9,7 @@ resource "azurerm_virtual_network" "region1-vnet1" {
   }
 }
 resource "azurerm_subnet" "region1-vnet1-snet1" {
-  name                = "${var.lab-name}-${var.region1}-vnet-01-snet-01"
+  name                 = "${var.lab-name}-${var.region1}-vnet-01-snet-01"
   resource_group_name  = azurerm_resource_group.rg1.name
   virtual_network_name = azurerm_virtual_network.region1-vnet1.name
   address_prefixes     = [var.region1-vnet1-snet1-range]
@@ -25,7 +25,7 @@ resource "azurerm_virtual_network" "region2-vnet1" {
   }
 }
 resource "azurerm_subnet" "region2-vnet1-snet1" {
-  name                = "${var.lab-name}-${var.region2}-vnet-01-snet-01"
+  name                 = "${var.lab-name}-${var.region2}-vnet-01-snet-01"
   resource_group_name  = azurerm_resource_group.rg2.name
   virtual_network_name = azurerm_virtual_network.region2-vnet1.name
   address_prefixes     = [var.region2-vnet1-snet1-range]

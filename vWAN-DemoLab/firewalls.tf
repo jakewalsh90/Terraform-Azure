f2c666ea8589cb0eeadffbb1bddd49715bf8507f
Jake Walsh <37653943+jakewalsh90@users.noreply.github.com>
2022-06-15 13:33:27 +0100

    Update firewalls.tf

--- vWAN-DemoLab/firewalls.tf
+++ vWAN-DemoLab/firewalls.tf
@@ -5,8 +5,9 @@ resource "azurerm_firewall" "region1-fw01" {
   resource_group_name = azurerm_resource_group.region1-rg1.name
   sku_tier            = "Premium"
   sku_name            = "AZFW_Hub"
+  firewall_policy_id  = azurerm_firewall_policy.fw-pol01.id
   virtual_hub {
-    virtual_hub_id = azurerm_virtual_hub.region1-vhub1.id
+    virtual_hub_id  = azurerm_virtual_hub.region1-vhub1.id
     public_ip_count = 1
   }
 }
@@ -16,8 +17,34 @@ resource "azurerm_firewall" "region2-fw01" {
   resource_group_name = azurerm_resource_group.region2-rg1.name
   sku_tier            = "Premium"
   sku_name            = "AZFW_Hub"
+  firewall_policy_id  = azurerm_firewall_policy.fw-pol01.id
   virtual_hub {
-    virtual_hub_id = azurerm_virtual_hub.region2-vhub1.id
+    virtual_hub_id  = azurerm_virtual_hub.region2-vhub1.id
     public_ip_count = 1
+  }
+}
+#Firewall Policy
+resource "azurerm_firewall_policy" "fw-pol01" {
+  name                = "fw-pol01"
+  resource_group_name = azurerm_resource_group.region1-rg1.name
+  location            = var.region1
+}
+# Firewall Policy Rules
+resource "azurerm_firewall_policy_rule_collection_group" "region1-policy1" {
+  name               = "fw-pol01-rules"
+  firewall_policy_id = azurerm_firewall_policy.fw-pol01.id
+  priority           = 100
+
+  network_rule_collection {
+    name     = "network_rules1"
+    priority = 100
+    action   = "Allow"
+    rule {
+      name                  = "network_rule_collection1_rule1"
+      protocols             = ["TCP", "UDP", "ICMP"]
+      source_addresses      = ["*"]
+      destination_addresses = ["*"]
+      destination_ports     = ["*"]
+    }
   }
 }

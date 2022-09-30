311b52256ad80593625ee0b835dbe5caf37d55fe
Jake Walsh <37653943+jakewalsh90@users.noreply.github.com>
2022-09-30 19:43:23 +0100

    Provider Updates

--- Azure-DevOps-WVD/Single-Region-BaseLab/provider.tf
+++ Azure-DevOps-WVD/Single-Region-BaseLab/provider.tf
@@ -15,7 +15,7 @@ terraform {
     azurerm = {
       # Specify what version of the provider we are going to utilise
       source  = "hashicorp/azurerm"
-      version = ">= 3.23.0"
+      version = ">= 3.25.0"
     }
     random = {
       source  = "hashicorp/random"

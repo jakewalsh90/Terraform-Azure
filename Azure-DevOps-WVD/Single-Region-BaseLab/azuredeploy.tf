33a29355575e202a06943a5f2470a8b54ac54660
Jake Walsh <37653943+jakewalsh90@users.noreply.github.com>
2022-10-06 07:52:18 +0100

    Centralise PowerShell

--- Azure-DevOps-WVD/Single-Region-BaseLab/azuredeploy.tf
+++ Azure-DevOps-WVD/Single-Region-BaseLab/azuredeploy.tf
@@ -327,7 +327,7 @@ resource "azurerm_virtual_machine_extension" "region1-dc01-basesetup" {
   settings = <<SETTINGS
     {
         "fileUris": [
-          "https://raw.githubusercontent.com/jakewalsh90/Terraform-Azure/main/Azure-DevOps-WVD/Single-Region-BaseLab/PowerShell/baselab_DCSetup.ps1"
+          "https://raw.githubusercontent.com/jakewalsh90/Terraform-Azure/main/PowerShell/baselab_DCSetup.ps1"
         ]
     }
   SETTINGS

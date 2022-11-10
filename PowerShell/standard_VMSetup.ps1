2678dfeccf8c3f36c7b5d02b132aeeab5f500cf4
Jake Walsh <37653943+jakewalsh90@users.noreply.github.com>
2022-11-10 09:28:26 +0000

    Update standard_VMSetup.ps1

--- PowerShell/standard_VMSetup.ps1
+++ PowerShell/standard_VMSetup.ps1
@@ -1,11 +1,10 @@
-#Set Execution Policy to allow script to run
+# Set Execution Policy to allow script to run
 Set-ExecutionPolicy Bypass -Scope Process -Force 
-#Choco install and Choco Apps
+# Choco install and Choco Apps
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 choco install googlechrome -y
 choco install putty -y
 choco install notepadplusplus -y
 choco install sysinternals -y
-#Allow Ping
-Set-NetFirewallRule -DisplayName "File and Printer Sharing (Echo Request - ICMPv4-In)" -enabled True
-Set-NetFirewallRule -DisplayName "File and Printer Sharing (Echo Request - ICMPv6-In)" -enabled True
+# Disable Firewall for Testing
+Set-NetFirewallProfile -All -Enabled False

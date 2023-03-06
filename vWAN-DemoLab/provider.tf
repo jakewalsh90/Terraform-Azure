1a89b4223846f634c6a246a83514bc17ce42dc1d
Jake Walsh <37653943+jakewalsh90@users.noreply.github.com>
2023-03-06 07:58:43 +0000

    Provider Updates to 3.46.0

--- vWAN-DemoLab/provider.tf
+++ vWAN-DemoLab/provider.tf
@@ -4,7 +4,7 @@ terraform {
     azurerm = {
       # Specify what version of the provider we are going to utilise
       source  = "hashicorp/azurerm"
-      version = ">= 3.41.0"
+      version = ">= 3.46.0"
     }
     random = {
       source  = "hashicorp/random"

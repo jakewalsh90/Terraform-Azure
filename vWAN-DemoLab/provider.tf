2809e21febdb1349e644c67a2350dd9f548bd54f
Jake Walsh <37653943+jakewalsh90@users.noreply.github.com>
2023-05-05 07:53:14 +0100

    Azurerm Provider update to 3.55.0
    Azurerm Provider update to 3.55.0

--- vWAN-DemoLab/provider.tf
+++ vWAN-DemoLab/provider.tf
@@ -4,7 +4,7 @@ terraform {
     azurerm = {
       # Specify what version of the provider we are going to utilise
       source  = "hashicorp/azurerm"
-      version = ">= 3.54.0"
+      version = ">= 3.55.0"
     }
     random = {
       source  = "hashicorp/random"

15578b6f73a561b26ea9693e3fa8e514e9cde1d9
Jake Walsh <37653943+jakewalsh90@users.noreply.github.com>
2023-06-06 09:09:29 +0100

    Updates to Chocolatey install

--- PowerShell/baselab_DCSetup.ps1
+++ PowerShell/baselab_DCSetup.ps1
@@ -1,7 +1,5 @@
-#Set Execution Policy to allow script to run
-Set-ExecutionPolicy Bypass -Scope Process -Force 
 #Choco install and Choco Apps
-iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 choco install googlechrome -y
 choco install putty -y
 choco install notepadplusplus -y

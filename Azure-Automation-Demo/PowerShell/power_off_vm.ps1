441fa2c9cf0b336ace871a9b11a8bb07a687a797
Jake Walsh <37653943+jakewalsh90@users.noreply.github.com>
2023-07-09 17:19:53 +0100

    Update power_off_vm.ps1

--- Azure-Automation-Demo/PowerShell/power_off_vm.ps1
+++ Azure-Automation-Demo/PowerShell/power_off_vm.ps1
@@ -1,12 +1,12 @@
 param (
     [Parameter(Mandatory=$true)]
-    [string]$DailyShutdownTime
+    [string]$dailyshutdowntime
 )
 
 Disable-AzContextAutosave -Scope Process
 $AzureContext = (Connect-AzAccount -Identity ).context
 $AzureContext = Set-AzContext -SubscriptionName $AzureContext.Subscription -DefaultProfile $AzureContext
 
-$VMs = Get-AzVM | Where-Object {$_.Tags.DailyShutdown -eq "$DailyShutdownTime"}
+$VMs = Get-AzVM | Where-Object {$_.Tags.DailyShutdown -eq "$dailyshutdowntime"}
 $VMs | Stop-AzVM -Force
 Write-Output "Stopped VMs are $($VMs.Name)"

be0910429ed6b57027036b542ec2bbc3a1263694
Jake Walsh <37653943+jakewalsh90@users.noreply.github.com>
2023-09-18 09:52:40 +0100

    azurerm provider updated to 3.73.0

--- Chaos-Studio-Test/provider.tf
+++ Chaos-Studio-Test/provider.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.72.0"
+      version = ">= 3.73.0"
     }
     random = {
       source  = "hashicorp/random"
@@ -17,6 +17,8 @@ provider "azurerm" {
     }
   }
 }
+
+
 
 
 
--- Terraform-Cloud-Demo-Lab/provider.tf
+++ Terraform-Cloud-Demo-Lab/provider.tf
@@ -24,3 +24,5 @@ provider "azurerm" {
   }
 }
 
+
+

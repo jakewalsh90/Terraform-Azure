e2621a1f555d95175186c39ff8f4f39ea34a20f0
Jake Walsh <37653943+jakewalsh90@users.noreply.github.com>
2023-10-07 10:25:21 +0100

    Update provider.tf

--- Terraform-Cloud-Demo-Lab/provider.tf
+++ Terraform-Cloud-Demo-Lab/provider.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "3.71.0"
+      version = ">= 3.75.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = "3.5.1"
+      version = ">= 3.5.1"
     }
   }
 }

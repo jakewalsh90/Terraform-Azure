5400ea1bb17477acaf33404fc0c1f52d2e0469ab
Jake Walsh <37653943+jakewalsh90@users.noreply.github.com>
2024-01-15 20:20:25 +0000

    AzureRM Provider updated to 3.87.0
    AzureRM Provider updated to 3.87.0

--- Cross-Region-Load-Balancer-Lab/provider.tf
+++ Cross-Region-Load-Balancer-Lab/provider.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.86.0"
+      version = ">= 3.87.0"
     }
     random = {
       source  = "hashicorp/random"

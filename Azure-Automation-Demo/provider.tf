a8af3972d4f797687d571839cc228593da0738a6
Jake Walsh <37653943+jakewalsh90@users.noreply.github.com>
2024-01-31 13:41:07 +0000

    AzureRM Provider updated to 3.89.0
    AzureRM Provider updated to 3.89.0

--- Azure-Automation-Demo/provider.tf
+++ Azure-Automation-Demo/provider.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.87.0"
+      version = ">= 3.89.0"
     }
   }
 }
--- Core-Environment-DemoLab-v1/provider.tf
+++ Core-Environment-DemoLab-v1/provider.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.87.0"
+      version = ">= 3.89.0"
     }
     random = {
       source  = "hashicorp/random"

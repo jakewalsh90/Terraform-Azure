c4895017dd3773f6ce39e0fa0e11098575136ef2
Jake Walsh <37653943+jakewalsh90@users.noreply.github.com>
2024-02-23 09:13:33 +0000

    AzureRM Provider update to 3.93.0
    AzureRM Provider update to 3.93.0

--- Core-Environment-DemoLab-v1/provider.tf
+++ Core-Environment-DemoLab-v1/provider.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.92.0"
+      version = ">= 3.93.0"
     }
     random = {
       source  = "hashicorp/random"

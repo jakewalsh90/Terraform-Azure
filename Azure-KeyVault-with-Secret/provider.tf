9c634dd67cca115dfbc4c681c3d586ab83b4f1dc
Jake Walsh <37653943+jakewalsh90@users.noreply.github.com>
2024-07-22 08:44:09 +0100

    AzureRM Provider updated to 3.113.0
    AzureRM Provider updated to 3.113.0

--- Azure-KeyVault-with-Secret/provider.tf
+++ Azure-KeyVault-with-Secret/provider.tf
@@ -4,7 +4,7 @@ terraform {
     azurerm = {
       # Specify what version of the provider we are going to utilise
       source  = "hashicorp/azurerm"
-      version = ">= 3.110.0"
+      version = ">= 3.113.0"
     }
     random = {
       source  = "hashicorp/random"

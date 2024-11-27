de28c601d82164d7b0c6d2707eabd4b166de86a2
Jake Walsh <37653943+jakewalsh90@users.noreply.github.com>
2024-11-27 12:38:49 +0000

    AzureRM Provider update to 4.11.0

--- Core-Environment-DemoLab-v1/provider.tf
+++ Core-Environment-DemoLab-v1/provider.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 4.4.0"
+      version = ">= 4.11.0"
     }
     random = {
       source  = "hashicorp/random"

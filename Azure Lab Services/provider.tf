2546663cdc0a017c42427d7ee185a8927222e5fe
Jake Walsh <37653943+jakewalsh90@users.noreply.github.com>
2025-04-21 15:15:07 +0100

    AzureRM Provider Update

--- Azure Lab Services/provider.tf
+++ Azure Lab Services/provider.tf
@@ -4,7 +4,7 @@ terraform {
     azurerm = {
       # Specify what version of the provider we are going to utilise
       source  = "hashicorp/azurerm"
-      version = ">= 4.24.0"
+      version = ">= 4.26.0"
     }
     random = {
       source  = "hashicorp/random"

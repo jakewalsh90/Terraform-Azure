bef6cdd4f8289e62f784aee70d1939a67259cd09
Jake Walsh <37653943+jakewalsh90@users.noreply.github.com>
2025-05-30 08:33:39 +0100

    Provider Updates 30052025
    Azurerm updated to 4.31.0, Random updated to 3.7.2

--- Core-Environment-DemoLab-v1/provider.tf
+++ Core-Environment-DemoLab-v1/provider.tf
@@ -3,11 +3,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 4.26.0"
+      version = ">= 4.31.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = ">= 3.7.1"
+      version = ">= 3.7.2"
     }
   }
 }
--- Getting Started Blog Post/provider.tf
+++ Getting Started Blog Post/provider.tf
@@ -3,7 +3,7 @@ terraform {
     azurerm = {
       # Specify what version of the provider we are going to utilise
       source  = "hashicorp/azurerm"
-      version = ">= 4.26.0"
+      version = ">= 4.31.0"
     }
     random = {
       source  = "hashicorp/random"
